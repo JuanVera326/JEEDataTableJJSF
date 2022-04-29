31ee29054d75587cb790117f393313a4da3cc0df
JuanVera326 <jjvera86@misena.edu.co>
2022-04-29 11:00:41 -0500

    Validaciones y estilos basicos

--- src/main/java/TablaBean.java
+++ src/main/java/TablaBean.java
@@ -9,16 +9,52 @@ public class TablaBean {
 	
 	private static ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
 	private Persona miPersona;
+	private String mensaje;
 	
 	public TablaBean() {
 		miPersona = new Persona();
 	}
 	
 	public void agregarPersona() {
-		listaPersonas.add(miPersona);
-		miPersona = new Persona();
+		boolean valid = validar();
+		if (valid == true) {
+			listaPersonas.add(miPersona);
+			miPersona = new Persona();
+		}else {
+			System.out.println("No va a entrar");
+		}
 	}
 	
+	public boolean validar() {
+		if (miPersona.getNombre().equals("")) {
+			setMensaje("Por Favor Ingrese un Nombre!!!");
+			return false;
+		}else if (miPersona.getApellido().equals("")) {
+			setMensaje("Por Favor Ingrese un Apellido!!!");
+			return false;
+		}else if (miPersona.getEdad() <= 0 || miPersona.getEdad() >= 120) {
+			setMensaje("Por Favor Ingrese un Edad Valida!!!");
+			return false;
+		}else if (miPersona.getProfesion().equals("")) {
+			setMensaje("Por Favor Ingrese un Profesion!!!");
+			return false;
+		}else if (miPersona.getSalario() <= 0) {
+			setMensaje("Por Favor Ingrese un Salario!!!");
+			return false;
+		}else {
+			setMensaje("");
+			return true;
+		}
+	}
+	
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String msj) {
+		this.mensaje = msj;
+	}
+
 	public void eliminarPersona(Persona miPersona) {
 		listaPersonas.remove(miPersona);
 	}
